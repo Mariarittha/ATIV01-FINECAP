c88cd54746f6c3e9b5beca2d3bc703fcb664bd5b
Mariarittha <Mariaritha91@gmailcom>
2023-09-04 13:21:33 -0300

    comitando

---
+++ reservas/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Stand, Reserva
+
+@admin.register(Reserva)
+class ReservaAdmin (admin.ModelAdmin):
+    list_display = ('cnpj','nome_empresa','categoria_empresa','quitado','imagem','localizacao','valor')
+
+
+
+
+@admin.register(Stand)
+class StandAdmin (admin.ModelAdmin):
+    list_display = ('localizacao','valor',)
+
