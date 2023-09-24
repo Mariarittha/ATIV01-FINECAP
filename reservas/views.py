34cf20f0e2d14c4b142005f9bcbed52ff3ca53cc
Mariarittha <Mariaritha91@gmailcom>
2023-09-23 22:33:39 -0300

    pequenas modifi...

--- reservas/views.py
+++ reservas/views.py
@@ -49,7 +49,7 @@ class Editar(views.SuccessMessageMixin, generic.UpdateView):
   form_class = ReservaForm
   template_name = "reservas/form.html"
   success_url = reverse_lazy("listar")
-  success_message = "Reserva atualizada com sucesso!"
+  success_message = "Reserva atualizada com sucesso! " 
   
   def login(request):
      if request.method == 'GET':
@@ -68,84 +68,3 @@ class Editar(views.SuccessMessageMixin, generic.UpdateView):
          return redirect('login')
      
 
-
-
-
-# def index(request):
-   
-#     return render(request,"reservas/index.html")
-
-# def mais(request):
-   
-#     return render(request,"reservas/mais.html")
-
-# def nos(request):
-   
-#     return render(request,"reservas/nos.html")
-
-# def cadastrar_reserva(request):
-#     if request.method == 'POST':
-#         form = ReservaForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             form = ReservaForm()
-            
-#     else:
-#         form = ReservaForm()
-    
-#     return render(request, "reservas/form.html", {'form':form})
-
-# def listar_reservas(request):
-#     reservas = Reserva.objects.all()
-#     context = {
-#         'reservas':reservas
-#     }
-#     return render (request, 'reservas/listar.html', context)
-
-
-# def detalhar(request, id):
-#     reservas = get_object_or_404(Reserva,id=id)
-#     context = {'reservas': reservas}
-#     return render(request,'reservas/detalhar.html', context)
-
-
-
-# def editar(request, id):
-#     reserva = get_object_or_404(Reserva, id=id)
-    
-#     if request.method =='POST':
-#         form = ReservaForm(request.POST,request.FILES,instance=reserva)
-        
-#         if form.is_valid():
-#             form.save()
-#             return redirect('listar')
-#     else:
-#         form = ReservaForm(instance=reserva)
-
-#     return render(request,'reservas/form.html',{'form':form})
-        
-
-# #apagar
-
-# def apagar(request, id):
-#     reserva = get_object_or_404(Reserva, id=id)
-#     reserva.delete()
-#     return redirect('listar')
-    
-# #login
-# def login(request):
-#      if request.method == 'GET':
-#          return render(request, 'Restrito/login.html')
-#      else:
-#          username = request.POST.get('username')
-#          senha = request.POST.get('senha')
-        
-#          user = authenticate(username = username, password = senha)
-        
-#          if user:
-#              loginho(request, user)
-#              return redirect('listar')
-#          else:
-#               messages.error(request,'username ou senha inválida') 
-#          return redirect('login')
-     
